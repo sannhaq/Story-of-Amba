a9fd8f415de0bc6772a435ff6401a919ca7e3606
sannhaq <ikhsannhaq@gmail.com>
2024-11-11 11:27:05 +0700

    feat: add some checkpoint feature

---
+++ helper.py
@@ -0,0 +1,34 @@
+import json
+import os
+import pygame
+import sys
+import threading
+import time
+
+CHECKPOINT_FILE = 'checkpoint.json' 
+
+def save_checkpoint(state):
+    """Menyimpan state permainan ke file checkpoint."""
+    with open(CHECKPOINT_FILE, 'w') as file:
+        json.dump(state, file)
+    print("\nCheckpoint tersimpan.")
+
+def load_checkpoint():
+    """Memuat state permainan dari file checkpoint jika ada."""
+    try:
+        with open(CHECKPOINT_FILE, 'r') as file:
+            state = json.load(file)
+        print("Checkpoint dimuat.")
+        return state
+    except FileNotFoundError:
+        print("Tidak ada checkpoint yang ditemukan.")
+        clear_console()
+        return None  # Mengembalikan None jika tidak ada checkpoint
+
+def delete_checkpoint():
+    """Menghapus file checkpoint jika ada."""
+    if os.path.exists(CHECKPOINT_FILE):
+        os.remove(CHECKPOINT_FILE)
+        print("Checkpoint dihapus.")
+    else:
+        print("Tidak ada checkpoint yang ditemukan untuk dihapus.")
