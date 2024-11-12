1514c5888ec328d8a63aa9011a009356247471a2
sannhaq <ikhsannhaq@gmail.com>
2024-11-12 11:42:46 +0700

    fix: structure folder story

--- src/story/chapter1.py
+++ src/story/chapter1.py
@@ -1,16 +1,3 @@
-def get_intro(nama_karakter):
-    return [
-        "========================================",
-        f"     Pulau Amba: Warisan Bajak Laut     ",
-        "========================================\n",
-        f"{nama_karakter}, seorang pemuda pesisir yang sederhana, menemukan kotak kayu tua di gudang rumahnya,",
-        "yang menyimpan surat rahasia dari leluhurnya, Kapten Alaric. Kapten ini dikenal sebagai bajak laut paling ditakuti",
-        "yang pernah berlayar di perairan mereka. Surat tersebut menggambarkan petunjuk menuju Pulau Amba,"
-        "sebuah pulau misterius tempat harta karun yang tak ternilai disembunyikan. Namun, harta tersebut diyakini dijaga oleh",
-        f"roh-roh dan kutukan yang hanya bisa dihadapi oleh jiwa yang berani dan tulus. {nama_karakter}, digerakkan oleh rasa",
-        "penasaran dan semangat petualangan, memulai perjalanan panjang yang menguji keberanian, kecerdasan, dan kemurnian hatinya."
-    ]
-
 def chapter_1(nama_karakter):
     return [
         f"Gemeretak pintu kayu tua dan aroma debu menyambut {nama_karakter} saat ia memasuki gudang rumah keluarganya.",
---
+++ src/story/opening.py
@@ -0,0 +1,12 @@
+def get_intro(nama_karakter):
+    return [
+        "========================================",
+        f"     Pulau Amba: Warisan Bajak Laut     ",
+        "========================================\n",
+        f"{nama_karakter}, seorang pemuda pesisir yang sederhana, menemukan kotak kayu tua di gudang rumahnya,",
+        "yang menyimpan surat rahasia dari leluhurnya, Kapten Alaric. Kapten ini dikenal sebagai bajak laut paling ditakuti",
+        "yang pernah berlayar di perairan mereka. Surat tersebut menggambarkan petunjuk menuju Pulau Amba,"
+        "sebuah pulau misterius tempat harta karun yang tak ternilai disembunyikan. Namun, harta tersebut diyakini dijaga oleh",
+        f"roh-roh dan kutukan yang hanya bisa dihadapi oleh jiwa yang berani dan tulus. {nama_karakter}, digerakkan oleh rasa",
+        "penasaran dan semangat petualangan, memulai perjalanan panjang yang menguji keberanian, kecerdasan, dan kemurnian hatinya."
+    ]
